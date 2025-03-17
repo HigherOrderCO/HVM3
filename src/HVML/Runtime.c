73b23ad97da14a7def9a8ff67c82e27f5589ed7f
wyattgill9 <wyattgill2009@gmail.com>
2025-03-17 08:17:50 -0700

    Added: MAP_NORESERVE to allow overcommit

--- src/HVML/Runtime.c
+++ src/HVML/Runtime.c
@@ -1426,7 +1426,8 @@ Term LOG_f(Term ref) {
 
 void *alloc_huge(size_t size) {
     void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE,
-                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_NOSERVE,
+                     -1, 0);
     if (ptr == MAP_FAILED) {
         perror("mmap failed");
         return NULL;
