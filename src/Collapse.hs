f274aa29289fcba1c9a26763a302791daf70c297
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-03-18 18:23:13 -0300

    new era rules, dfs, bfs, flattening

--- src/Collapse.hs
+++ src/Collapse.hs
@@ -18,6 +18,26 @@ import System.IO.Unsafe (unsafeInterleaveIO)
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as MS
 
+--  #C{* * *}
+-- -------------- CTR-ERA
+-- *
+icEraCtr :: Term -> Term -> HVM Term
+icEraCtr ctr _ = do
+  let ctrLoc = termLoc ctr
+  let eraTerm = termNew _ERA_ 0 0
+  setOld ctrLoc (termSetBit eraTerm)
+  return eraTerm
+
+-- ~ * { K0 K1 K2 ... }
+-- --------------------- MAT-ERA
+-- *
+icEraMat :: Term -> Term -> HVM Term
+icEraMat mat _ = do
+  let matLoc = termLoc mat
+  let eraTerm = termNew _ERA_ 0 0
+  setOld matLoc (termSetBit eraTerm)
+  return eraTerm
+
 -- λx.*
 -- ------ ERA-LAM
 -- x <- *
@@ -312,162 +332,51 @@ icSupCtr ctr sup ctrAri supIdx = do
   setNew (newSupLoc + 1) ctr1
   return (termNew _SUP_ supLab newSupLoc)
 
-
-
 -- ~N{#A: &L{z0 z1} #B: x #C: y ...}
 -- --------------------------------- SUP-MAT-CTR
 -- !&L{N0,N1} = N;
 -- !&L{x0,x1} = x;
 -- !&L{y0,y1} = y;
 -- &L{~N0{#A:z0 #B: x0 #C: y0} ~N1{#A:z1 #B: x1 #C: y1}}
-icSupMatCtr :: Term -> Term -> Word64 -> Word64 -> HVM Term
-icSupMatCtr mat sup matLen supIdx = do
+icSupMatCtr :: Tag -> Term -> Term -> Word64 -> Word64 -> HVM Term
+icSupMatCtr matTag mat sup matLen supIdx = do
   let matLoc = termLoc mat
   let matLab = termLab mat
-  let supLab = termLab sup 
+  let supLab = termLab sup
   let supLoc = termLoc sup
-
-  -- Step 1: Get N from the MAT term and duplicate it
   n <- got (matLoc + 0)
   dupNLoc <- allocNode 1
   setNew dupNLoc n
   let n0 = termNew _DP0_ supLab dupNLoc
   let n1 = termNew _DP1_ supLab dupNLoc
-
-  -- Step 2: Get the superposition components from sup
   z0 <- got (supLoc + 0)
   z1 <- got (supLoc + 1)
-
-  -- Step 3: Process all case terms, duplicating non-superposed ones
   uList <- forM [1 .. matLen] $ \j -> do
     let caseOffset = matLoc + j
-    if j == supIdx then do
-      -- This is the superposed case; use z0 and z1 directly
-      return (z0, z1)
+    if j == supIdx then return (z0, z1)
     else do
-      -- Duplicate non-superposed case
       tj <- got caseOffset
-
       dupTjLoc <- allocNode 1
       setNew dupTjLoc tj
       let t0j = termNew _DP0_ supLab dupTjLoc
       let t1j = termNew _DP1_ supLab dupTjLoc
       return (t0j, t1j)
-
-  -- Step 4: Create the first MAT term
   mat0Loc <- allocNode (matLen + 1)
   setNew (mat0Loc + 0) n0
   forM_ [0 .. matLen - 1] $ \j -> do
     let (u_j, _) = uList !! fromIntegral j
     setNew (mat0Loc + 1 + j) u_j
-  let mat0 = termNew _MAT_ matLab mat0Loc
-
-  -- Step 5: Create the second MAT term
+  let mat0 = termNew matTag matLab mat0Loc
   mat1Loc <- allocNode (matLen + 1)
   setNew (mat1Loc + 0) n1
   forM_ [0 .. matLen - 1] $ \j -> do
     let (_, v_j) = uList !! fromIntegral j
     setNew (mat1Loc + 1 + j) v_j
-  let mat1 = termNew _MAT_ matLab mat1Loc
-
-  -- Step 6: Create the resulting superposition
+  let mat1 = termNew matTag matLab mat1Loc
   supLoc <- allocNode 2
   setNew (supLoc + 0) mat0
   setNew (supLoc + 1) mat1
   return (termNew _SUP_ supLab supLoc)
-
-      
--- ~N{0:&L{z0,z1};+:s;}
--- --------------------------------- SUP-SWI-Z
--- !&L{N0,N1} = N;
--- !&L{S0,S1} = S;
--- &L{~N0{0:z0;+:S0},~N1{0:z1;+:S1}}
-icSupSwiZ :: Term -> Term -> HVM Term
-icSupSwiZ swi sup = do
-  let swiLoc = termLoc swi
-  let supLoc = termLoc sup
-  let supLab = termLab sup
-
-  num <- got (swiLoc + 0)
-  z0  <- got (supLoc + 0)
-  z1  <- got (supLoc + 1)
-  s   <- got (swiLoc + 2)
-
-  dupNLoc <- allocNode 1
-  dupSLoc <- allocNode 1
-
-  setNew dupNLoc num
-  setNew dupSLoc s
-
-  let n0 = termNew _DP0_ supLab dupNLoc
-  let n1 = termNew _DP1_ supLab dupNLoc
-  let s0 = termNew _DP0_ supLab dupSLoc
-  let s1 = termNew _DP1_ supLab dupSLoc
-
-  swi0Loc <- allocNode 3
-  setNew (swi0Loc + 0) n0
-  setNew (swi0Loc + 1) z0
-  setNew (swi0Loc + 2) s0
-
-  swi1Loc <- allocNode 3
-  setNew (swi1Loc + 0) n1
-  setNew (swi1Loc + 1) z1
-  setNew (swi1Loc + 2) s1
-
-  let swi0 = termNew _SWI_ 0 swi0Loc
-  let swi1 = termNew _SWI_ 0 swi1Loc
-
-  resLoc <- allocNode 2
-  setNew (resLoc + 0) swi0
-  setNew (resLoc + 1) swi1
-
-  return (termNew _SUP_ supLab resLoc)
-
--- ~N{0:z;+:&0{s0,s1};}
--- --------------------------------- SUP-SWI-S
--- !&L{N0,N1} = N;
--- !&L{Z0,Z1} = Z;
--- &L{~N0{0:z0;+:S0},~N1{0:z1;+:S1}}
-icSupSwiS :: Term -> Term -> HVM Term
-icSupSwiS swi sup = do
-  let swiLoc = termLoc swi
-  let supLoc = termLoc sup
-  let supLab = termLab sup
-
-  num <- got (swiLoc + 0)
-  z   <- got (swiLoc + 1)
-  s0  <- got (supLoc + 0)
-  s1  <- got (supLoc + 1)
-
-  dupNLoc <- allocNode 1
-  dupZLoc <- allocNode 1
-
-  setNew dupNLoc num
-  setNew dupZLoc z
-
-  let n0 = termNew _DP0_ supLab dupNLoc
-  let n1 = termNew _DP1_ supLab dupNLoc
-  let z0 = termNew _DP0_ supLab dupZLoc
-  let z1 = termNew _DP1_ supLab dupZLoc
-
-  swi0Loc <- allocNode 3
-  setNew (swi0Loc + 0) n0
-  setNew (swi0Loc + 1) z0
-  setNew (swi0Loc + 2) s0
-
-  swi1Loc <- allocNode 3
-  setNew (swi1Loc + 0) n1
-  setNew (swi1Loc + 1) z1
-  setNew (swi1Loc + 2) s1
-
-  let swi0 = termNew _SWI_ 0 swi0Loc
-  let swi1 = termNew _SWI_ 0 swi1Loc
-
-  resLoc <- allocNode 2
-  setNew (resLoc + 0) swi0
-  setNew (resLoc + 1) swi1
-
-  return (termNew _SUP_ supLab resLoc)
 
 isDup :: TAG -> Bool
 isDup tag =
@@ -484,14 +393,10 @@ isSup term = termTag term == _SUP_
 
 collapseSupsTerm :: Book -> Term -> HVM Term
 collapseSupsTerm book root = do
-  -- Step 1: Reduce to WHNF
   term <- reduceC root 0
-
   let tag = termTag term
   let lab = termLab term
   let loc = termLoc term
-
-  -- Step 2: Recursively collapse children
   case (tagT tag) of
     LAM -> do
       bod <- got (loc + 0)
@@ -519,28 +424,21 @@ collapseSupsTerm book root = do
         ctrCol <- collapseSupsTerm book ctr
         setOld (loc + (i - 1)) ctrCol
       return ()
-
-    _ -> do return ()
-
-  -- Step 3: Reduce to WHNF again
+    _ -> return ()
   term <- reduceC term 0
   let tag = termTag term
   let lab = termLab term
   let loc = termLoc term
-
-  -- Step 4: Apply interaction rules
   case (tagT tag) of
     LAM -> do
       bodCol <- got (loc + 0)
       if isSup bodCol then do
         result <- icSupLam term bodCol
         collapseSupsTerm book result
-
       else if isEra bodCol then do
         result <- icEraLam term bodCol
         collapseSupsTerm book result
-      else
-        return term
+      else return term
     APP -> do
       funCol <- got (loc + 0)
       argCol <- got (loc + 1)
@@ -550,105 +448,76 @@ collapseSupsTerm book root = do
       else if isEra argCol then do
         result <- icEraApp term argCol
         collapseSupsTerm book result
-      else
-        return term
+      else return term
     SUP -> do
       lftCol <- got (loc + 0)
       rgtCol <- got (loc + 1)
-
       if isSup lftCol && lab > termLab lftCol then do
         result <- icSupSupX term lftCol
         collapseSupsTerm book result
-
       else if isSup rgtCol && lab > termLab rgtCol then do
         result <- icSupSupY term rgtCol
         collapseSupsTerm book result
+      else if isEra lftCol && isEra rgtCol then do
+        
       else return term
-
-    SWI -> do
-      _ <- got (loc + 0)  -- num (unused in interactions)
-      ifz <- got (loc + 1)
-      ifs <- got (loc + 2)
-      if isSup ifz then do
-        result <- icSupSwiZ term ifz
-        collapseSupsTerm book result
-      else if isSup ifs then do
-        result <- icSupSwiS term ifs
-        collapseSupsTerm book result
-      else
-        return term
-
-
-    MAT -> do
-        let matLen = mget (cidToLen book) lab
-
-        result <- let findFirstSup i = do
-                        if i > matLen 
-                          then return term 
-                          else do
-                            trm <- got (loc + (i - 1))
-                            if isSup trm 
-                              then do
-                                res <- icSupMatCtr term trm matLen (i - 1)
-                                collapseSupsTerm book res 
-                              else findFirstSup (i + 1)
-                  in findFirstSup 1
-
-        return result
-
     CTR -> do
-          let cid = termLab term
-          let ctrAri = mget (cidToAri book) cid
-          let findFirstSup i = if i >= ctrAri then return term else do
-                field <- got (loc + i)
-                if isSup field then do
-                  res <- icSupCtr term field ctrAri i
-                  collapseSupsTerm book res
-                else findFirstSup (i + 1)
-          findFirstSup 0
+      let cid = termLab term
+      let ctrAri = mget (cidToAri book) cid
+      let checkEraOrSup i = if i >= ctrAri then return term else do
+            field <- got (loc + i)
+            if isEra field then do
+              res <- icEraCtr term field
+              collapseSupsTerm book res
+            else if isSup field then do
+              res <- icSupCtr term field ctrAri i
+              collapseSupsTerm book res
+            else checkEraOrSup (i + 1)
+      checkEraOrSup 0
 
-    _ -> do
-      return term
+    tag | tag == MAT || tag == SWI -> do
+      let matLen = if tag == SWI then 2 else mget (cidToLen book) lab
+      let matTag = if tag == SWI then _SWI_ else _MAT_
+      let checkEraOrSup i = if i > matLen then return term else do
+            trm <- got (loc + i)
+            if isEra trm then do
+              res <- icEraMat term trm
+              collapseSupsTerm book res
+            else if isSup trm then do
+              res <- icSupMatCtr matTag term trm matLen i
+              collapseSupsTerm book res
+            else checkEraOrSup (i + 1)
+      checkEraOrSup 1
+    _ -> return term
+
 
 collapseDupsTerm :: Book -> Term -> HVM Term
 collapseDupsTerm book root = do
-  -- Reduce the term to weak head normal form (WHNF)
   term <- reduceC root 0
-
   let tag = termTag term
   let loc = termLoc term
-
   case (tagT tag) of
     tag | isDup tag -> do
-      -- Get the value this duplication points to and collapse it
       val <- got loc
       valCol <- collapseDupsTerm book val
       let valTag = termTag valCol
-
       case (tagT valTag) of
         VAR -> do
           result <- icDupVar term valCol
           collapseDupsTerm book result
-
         APP -> do
           result <- icDupApp term valCol
           collapseDupsTerm book result
-
         ERA -> do
           result <- icDupEra term valCol
           collapseDupsTerm book result
-
         _ -> return term
-
     LAM -> do
-      -- Collapse the body of the lambda
       bod <- got (loc + 0)
       bodCol <- collapseDupsTerm book bod
       setOld (loc + 0) bodCol
       return term
-
     APP -> do
-      -- Collapse the function and argument of the application
       fun <- got (loc + 0)
       arg <- got (loc + 1)
       funCol <- collapseDupsTerm book fun
@@ -656,9 +525,7 @@ collapseDupsTerm book root = do
       setOld (loc + 0) funCol
       setOld (loc + 1) argCol
       return term
-
     SUP -> do
-      -- Collapse the left and right subterms of the superposition
       lft <- got (loc + 0)
       rgt <- got (loc + 1)
       lftCol <- collapseDupsTerm book lft
@@ -666,54 +533,57 @@ collapseDupsTerm book root = do
       setOld (loc + 0) lftCol
       setOld (loc + 1) rgtCol
       return term
-
-    OPX -> do 
-      -- Collapse the child of the successor
+    OPX -> do
       child <- got (loc + 0)
       childCol <- collapseDupsTerm book child
       setOld (loc + 0) childCol
       return term
-
-    SWI -> do
-      -- Collapse the number, if-zero, and if-successor branches of the switch
-      num <- got (loc + 0)
-      ifz <- got (loc + 1)
-      ifs <- got (loc + 2)
-      numCol <- collapseDupsTerm book num
-      ifzCol <- collapseDupsTerm book ifz
-      ifsCol <- collapseDupsTerm book ifs
-      setOld (loc + 0) numCol
-      setOld (loc + 1) ifzCol
-      setOld (loc + 2) ifsCol
-      return term
-
     CTR -> do
-      -- Collapse all the fields of the constructor
-      let cid = termLab term 
+      let cid = termLab term
       let ctrAri = mget (cidToAri book) cid
       forM_ [1 .. ctrAri] $ \i -> do
         ctr <- got (loc + (i - 1))
         ctrCol <- collapseDupsTerm book ctr
         setOld (loc + (i - 1)) ctrCol
-
       return term
-
-    MAT -> do
-      -- Get the constructor ID and number of cases
-      let cid = termLab term
-      let matLen = mget (cidToLen book) cid
-      -- Collapse the scrutinee
+    tag | tag == MAT || tag == SWI -> do
+      let matLen = if tag == SWI then 2 else mget (cidToLen book) (termLab term)
       scrutinee <- got (loc + 0)
       scrutineeCol <- collapseDupsTerm book scrutinee
       setOld (loc + 0) scrutineeCol
-      -- Collapse each case
       forM_ [1 .. matLen] $ \i -> do
         caseTerm <- got (loc + i)
         caseCol <- collapseDupsTerm book caseTerm
         setOld (loc + i) caseCol
       return term
-  
     ERA -> return term
     W32 -> return term
     _   -> return term
 
+flattenDFS :: Core -> [Core]
+flattenDFS (Sup lab a b) = flattenDFS a ++ flattenDFS b
+flattenDFS Era = []
+flattenDFS x = [x]
+
+data SQ a = SQ [a] [a]
+
+sqPop :: SQ a -> Maybe (a, SQ a)
+sqPop (SQ [] [])     = Nothing
+sqPop (SQ [] ys)     = sqPop (SQ (reverse ys) [])
+sqPop (SQ (x:xs) ys) = Just (x, SQ xs ys)
+
+sqPut :: a -> SQ a -> SQ a
+sqPut x (SQ xs ys) = SQ xs (x:ys)
+
+flattenBFS :: Core -> [Core]
+flattenBFS root = bfs (sqPut root (SQ [] [])) []
+  where
+    bfs :: SQ Core -> [Core] -> [Core]
+    bfs sq acc =
+      case sqPop sq of
+        Nothing -> reverse acc
+        Just (node, sq') ->
+          case node of
+            Sup _ a b -> bfs (sqPut b (sqPut a sq')) acc
+            Era -> bfs sq' acc
+            x -> bfs sq' (x : acc)
